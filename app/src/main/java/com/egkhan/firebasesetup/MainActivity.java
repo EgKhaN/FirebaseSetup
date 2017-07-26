03755a14bd4bda0eab496b49b3fc7b189cebbba0
egkhan <egkarahan@gmail.com>
2017-07-26 13:31:09 +0300

    - Başlangıç ayarları yapıldı - Login ekranı yapıldı

---
+++ app/src/main/java/com/egkhan/firebasesetup/MainActivity.java
@@ -0,0 +1,89 @@
+package com.egkhan.firebasesetup;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    EditText emailET,passwordET;
+    Button signInBtn,signOutBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        emailET = (EditText)findViewById(R.id.email);
+        passwordET = (EditText)findViewById(R.id.password);
+        signInBtn = (Button) findViewById(R.id.email_sign_in_button);
+        signOutBtn = (Button) findViewById(R.id.email_sign_out_button);
+        
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailET.getText().toString();
+                String password = passwordET.getText().toString();
+                if (!email.equals("") && ! password.equals(""))
+                {
+                    mAuth.signInWithEmailAndPassword(email,password);
+                }
+                else
+                {
+                    ToastMessage("You didnt fill in all the fields");
+                }
+            }
+        });
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                ToastMessage("Signing Out...");
+            }
+        });
+        
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    ToastMessage("Successfully sign in with "+user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    ToastMessage("Signed out successfully");
+                }
+                // ...
+            }
+        };
+        
+    }
+    
+    private void ToastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
